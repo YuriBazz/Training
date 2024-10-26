b7f95a71e2469078f0d28d9b0eff63cf50982247
Yuri Kovach <gim.ekb12@gmail.com>
2024-10-26 15:31:36 +0500

    123

---
+++ CF_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+namespace CF_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = ReadLongs();
+            var fr = new (long, long)[t[0]];
+            for(int i = 0; i < t[0]; i++)
+            {
+                var t1 = ReadLongs();
+                fr[i] = (t1[0], t1[1]);
+            }
+            fr.OrderBy(x => x.Item1).ToArray();
+            long g = 0;
+            long temp = 0;
+            long minMoney = 0;
+            for(int r = 0; r < t[0]; r++)
+            {
+                if(minMoney == 0 || fr[r].Item1 <= minMoney)
+                    minMoney = fr[r].Item1;
+                if()
+            }
+        }
+
+        static long[] ReadLongs()
+        {
+            return Console.ReadLine().Split().Select(long.Parse).ToArray();
+        }
+    }
+}
