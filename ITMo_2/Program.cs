1175f7ed99a45b0800230e5e5c331a2fcae173fb
George Maslov <gim.ekb12@gmail.com>
2024-10-29 14:55:28 +0500

    123342

--- ITMo_2/Program.cs
+++ ITMo_2/Program.cs
@@ -11,38 +11,31 @@ namespace ITMo_2
         static void Main(string[] args)
         {
             var t = int.Parse(Console.ReadLine());
-            for (var i = 0; i < t; ++i)
+            for (; t > 0; --t)
             {
-                var temp = Console.ReadLine();
-                int[] z1 = ZFunction(temp), z2 = ZFunction(new string(temp.Reverse().ToArray()));
+                var s = Console.ReadLine();
+                var sr = new string(s.Reverse().ToArray());
+                var pr = new HashSet<string>();
+                var sf = new HashSet<string>();
                 var res = 0;
-                for (var j = 0; j < z1.Length; ++j)
+                for (var i = 1; i <= s.Length; ++i)
                 {
-                    res += z1[i] * z2[i] == 0 ? z1[i] + z2[i] : 0;
+                    pr.Add(s.Substring(0, i));
+                    sf.Add(s.Substring(i, s.Length - i));
+                }
+
+                pr.Remove(s);
+
+                for (var i = 0; i < s.Length; ++i)
+                {
+                    for (var j = 1; j <= s.Length - i; ++j)
+                    {
+                        var sub = s.Substring(i, j);
+                        if (pr.Contains(sub) ^ sf.Contains(sub)) res++;
+                    }
                 }
                 Console.WriteLine(res);
             }
-        }
-        
-        static int[] ZFunction(string s)
-        {
-            var z = new int[s.Length];
-            int l = 0, r = 0;
-            for (var i = 1; i < s.Length; ++i)
-            {
-                if (r >= i)
-                    z[i] = Math.Min(z[i - l], r - i + 1);
-
-                while (z[i] + i < s.Length && s[z[i]] == s[z[i] + i])
-                    z[i]++;
-                
-                if (i + z[i] - 1 > r)
-                {
-                    l = i;
-                    r = z[i] + i - 1;
-                }
-            }
-            return z;
         }
     }
 }
