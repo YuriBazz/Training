04416c55063ce4b953ba37bb9420767e5b4de2da
Yuri Kovach <gim.ekb12@gmail.com>
2024-10-29 19:21:09 +0500

    commit

---
+++ ITMO_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITMO_3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = int.Parse(Console.ReadLine());
+            for (; q > 0; --q)
+            {
+                var t = Console.ReadLine();
+                var p = Console.ReadLine();
+                var r = new List<int>();
+                for (var i = 0; i <= t.Length - p.Length; ++i)
+                    if (Compare(t.Substring(i, p.Length), p))
+                        r.Add(i);
+                Write(r);
+            }
+        }
+
+        static bool Compare(string a, string b)
+        {
+            return !a.Where((t, i) => t != b[i] && b[i] != '?').Any();
+        }
+
+        static void Write(List<int> list)
+        {
+            Console.WriteLine(list.Count);
+            var s = new StringBuilder();
+            s.AppendJoin(' ', list);
+            Console.WriteLine(s.ToString());
+        }
+    }
+}
---
+++ ITMO_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Linq;
+using System.Text;
+
+namespace ITMO_4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = int.Parse(Console.ReadLine());
+            for (; q > 0; --q)
+            {
+                var s = Console.ReadLine();
+                var t = Console.ReadLine();
+                var r = (s.Length * s.Length + s.Length) / 2;
+
+            }
+        }
+
+        static int Include(string s, string t)
+        {
+            var r = 0;
+            for(var i = 0; i <= s.Length - t.Length; ++i)
+                if (s.Substring(i, t.Length) == t)
+                    r += s.Length - t.Length - i + 1;
+            return r;
+        }
+        
+    }
+}
