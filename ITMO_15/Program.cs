edcbcd973aa8987189e580989be1e12489ec1092
George Maslov <gim.ekb12@gmail.com>
2024-11-14 13:13:25 +0500

    Commit

---
+++ ITMO_15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITMO_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]), m = int.Parse(input[1]);
+        }
+    }
+}
