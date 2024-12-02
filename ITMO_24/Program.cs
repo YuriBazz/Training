b1973eab6f4977a6465901851425fa44778b3337
Yuri Kovach <gim.ekb12@gmail.com>
2024-12-02 17:11:25 +0500

    Commit

---
+++ ITMO_24/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITMO_24
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
