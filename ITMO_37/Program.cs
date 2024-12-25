1b9943b866e85c1bfd8b0f10cc1527cb41a40318
George Maslov <gim.ekb12@gmail.com>
2024-12-25 23:54:21 +0500

    Commit

--- ITMO_37/Program.cs
+++ ITMO_37/Program.cs
@@ -10,11 +10,40 @@ namespace ITMO_37
         static void Main(string[] args)
         {
             var token = Console.ReadLine().Split(' ');
-            var n = int.Parse(token[0]);
             var k =  long.Parse(token[1]);
-            long left = 0, right = n * n + 1;
+            /*if (k == 1)
+            {
+                Console.WriteLine(1);
+                return;
+            }*/
+            var n = int.Parse(token[0]);
+            /*for (var i = 0; i < n * n; ++i)
+            {
+                Console.Write(BS(n,i) + " ");
+            }*/
+            Console.WriteLine(BS(n, k - 1));
         }
         
-        
+        static long BS(int n, long k)
+        {
+            long left = 1, right = (long)n * n + 1;
+            while (right > left + 1)
+            {
+                var x = left + (right - left) / 2;
+                var count = 0L;
+                var div = 0L;
+                for (var i = 1L; i <= n; ++i)
+                {
+                    count += Math.Min(x / i, n);
+                    if (x % i == 0 && x / i <= n)
+                        div++;
+                }
+                count -= div;
+                if (count <= k) left = x;
+                else right = x;
+            }
+
+            return left;
+        }
     }
 }
---
+++ ITMO_38/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITMO_38
+{
+
+    class Program
+    {
+        static int[] ReadInts() => Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        static void Main(string[] args)
+        {
+            var token = Console.ReadLine().Split(" ");
+            var n = int.Parse(token[0]);
+            var k = long.Parse(token[1]);
+            int[] a = ReadInts(), b = ReadInts();
+            Array.Sort(a);
+            
+            long left = -1, right = (long)2e9 + 1;
+            while (right > left + 1)
+            {
+                var middle = left + (right - left) / 2;
+                var sum = 0L;
+                for (var i = 0; i < n; ++i)
+                    sum += InsideBS(a, middle - b[i]) + 1;
+                if (sum <= k - 1) left = middle;
+                else right = middle;
+            }
+            Console.WriteLine(left + 1);
+        }
+
+        static int InsideBS(int[] array, long value)
+        {
+            int left = -1, right = array.Length;
+            while (right > left + 1)
+            {
+                var middle = left + (right - left) / 2;
+                if (array[middle] <= value) left = middle;
+                else right = middle;
+            }
+            return left;
+        }
+    }
+}
