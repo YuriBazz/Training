a09fdcacb99c8d74162a82b634e6be9bb4942a63
George Maslov <gim.ekb12@gmail.com>
2025-01-02 20:22:31 +0500

    Commit

---
+++ ITMO_39/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITMO_39
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var count = int.Parse(Console.ReadLine()); count > 0; --count)
+            {
+                Console.ReadLine();
+                var dic = new Dictionary<int, HashSet<int>>();
+                var f = false;
+                for (var m = Console.ReadLine().Split(" ").Select(int.Parse).ToArray()[1]; m > 0; --m)
+                {
+                    var token = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                    var (u, v) = (token[0], token[1]);
+                    if ((dic.ContainsKey(u) && dic[u].Contains(v)) || (dic.ContainsKey(v) && dic[v].Contains(u)) || u == v)
+                    {
+                        f = true;
+                    }
+                    if (!dic.ContainsKey(u)) dic[u] = new HashSet<int>();
+                    if (!dic.ContainsKey(v)) dic[v] = new HashSet<int>();
+                    dic[u].Add(v);
+                    dic[v].Add(u);
+                }
+                if(f) Console.WriteLine("NO");
+                else Console.WriteLine("YES");
+            }
+        }
+    }
+}
---
+++ ITMO_40/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITMO_40
+{
+    class Program
+    {
+        static int[] ReadInts() => Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        static void Print(int[] array) => Console.WriteLine(string.Join(" ", array));
+        
+        static void Main(string[] args)
+        {
+            for (var t = int.Parse(Console.ReadLine()); t > 0; --t)
+            {
+                Console.ReadLine();
+                var nm = ReadInts();
+                var vertexes = new int[nm[0]];
+                for (var m = nm[1]; m > 0; --m)
+                {
+                    var edge = ReadInts();
+                    vertexes[edge[0] - 1]++;
+                    vertexes[edge[1] - 1]++;
+                }
+                Print(vertexes);
+            }
+        }
+    }
+}
