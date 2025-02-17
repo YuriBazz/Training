db47a6a3b91d8b5d126d758ea26ffc2d3b5fe509
George Maslov <gim.ekb12@gmail.com>
2025-02-17 14:00:58 +0500

    Commit

--- ITMO_62/Program.cs
+++ ITMO_62/Program.cs
@@ -2,31 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 
-// TODO: Not working
 namespace ITMO_62
 {
     static class Program
     {
-        static int[] Read() => Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        static List<int> Read() => Console.ReadLine().Split(" ").Select(int.Parse).ToList();
         
         static void Main(string[] args)
         {
             var token = Read();
-            var (s, A, B) = (token[2], token[3], token[4]);
-            var a = Read().Select(x => (x, A)).ToArray();
-            Array.Sort(a);
-            var b = Read().Select(x => (x, B)).ToArray();
-            Array.Sort(b, (x,y) => -x.CompareTo(y));
+            var (s, A, B) = ((long)token[2], token[3], token[4]);
+            var a = Read().Select(x => (x, A)).ToList();
+            a.Sort();
+            var b = Read().Select(x => (x,B)).ToList();
+            b.Sort((x,y) => -x.CompareTo(y));
+            a.AddRange(b);
             var res = 0L;
-            for (var (l, r, w, c) = (0, 0, 0L, 0L); r < Math.Max(a.Length, b.Length); ++r)
+            for (var (l, r, c, w) = (0, 0, 0L, 0L); r < a.Count; r++)
             {
-                
+                c += a[r].x;
+                w += a[r].A;
+                while (w > s)
+                {
+                    w -= a[l].A;
+                    c -= a[l++].x;
+                }
+
+                if (w <= s) res = Math.Max(res, c);
             }
-            
             Console.WriteLine(res);
-            var list = new List<int>();
-            for(var i = 0L; i < 2e10; ++i)
-                list.Add(1);
         }
     }
 }
--- YA_1/Program.cs
+++ YA_1/Program.cs
@@ -1,91 +1,101 @@
-﻿namespace YA_1;
+﻿using System;
+using System.Globalization;
 
-class Program
+namespace YA_1
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        double l = 30.0, r = 4000.0, prev = 0;
-        for (var n = int.Parse(Console.ReadLine()); n > 0; --n)
+        static void Main(string[] args)
         {
-            var inp = Console.ReadLine().Split(" ");
-            if (inp.Length == 1)
+            double l = 30.0, r = 4000.0, prev = 0;
+            for (var n = int.Parse(Console.ReadLine()); n > 0; --n)
             {
-                prev = double.Parse(inp[0]);
-                continue;
+                var inp = Console.ReadLine().Split(" ");
+                if (inp.Length == 1)
+                {
+                    prev = double.Parse(inp[0], CultureInfo.InvariantCulture);
+                    continue;
+                }
+
+                var curr = double.Parse(inp[0], CultureInfo.InvariantCulture);
+                switch (inp[1])
+                {
+                    case "closer":
+                    {
+                        if (Math.Abs(l - prev) < Math.Abs(l - curr))
+                        {
+                            var tempR = r;
+                            var tempL = l;
+                            for (var i = 0; i < 100; ++i)
+                            {
+                                var mid = tempL + (tempR - tempL) / 2;
+                                if (Math.Abs(mid - prev) >= Math.Abs(mid - curr))
+                                    tempR = mid;
+                                else tempL = mid;
+                            }
+
+                            l = tempR;
+                        }
+
+                        if (Math.Abs(r - prev) < Math.Abs(r - curr))
+                        {
+                            var tempR = r;
+                            var tempL = l;
+                            for (var i = 0; i < 100; ++i)
+                            {
+                                var mid = tempL + (tempR - tempL) / 2;
+                                if (Math.Abs(mid - prev) > Math.Abs(mid - curr))
+                                    tempL = mid;
+                                else tempR = mid;
+                            }
+
+                            r = tempL;
+                        }
+
+                        break;
+                    }
+                    case "further":
+                    {
+                        if (Math.Abs(l - prev) > Math.Abs(l - curr))
+                        {
+                            var tempR = r;
+                            var tempL = l;
+                            for (var i = 0; i < 100; ++i)
+                            {
+                                var mid = tempL + (tempR - tempL) / 2;
+                                if (Math.Abs(mid - prev) < Math.Abs(mid - curr))
+                                    tempR = mid;
+                                else tempL = mid;
+                            }
+
+                            l = tempR;
+                        }
+
+                        if (Math.Abs(r - prev) > Math.Abs(r - curr))
+                        {
+                            var tempR = r;
+                            var tempL = l;
+                            for (var i = 0; i < 100; ++i)
+                            {
+                                var mid = tempL + (tempR - tempL) / 2;
+                                if (Math.Abs(mid - prev) < Math.Abs(mid - curr))
+                                    tempL = mid;
+                                else tempR = mid;
+                            }
+
+                            r = tempL;
+                        }
+
+                        break;
+                    }
+
+                }
+
+                prev = curr;
             }
 
-            var curr = double.Parse(inp[0]);
-            switch (inp[1])
-            {
-                case "closer":
-                {
-                    if(Math.Abs(l - prev) < Math.Abs(l - curr))
-                    {
-                        var tempR = r;
-                        var tempL = l;
-                        for (var i = 0; i < 100; ++i)
-                        {
-                            var mid = tempL + (tempR - tempL) / 2;
-                            if (Math.Abs(mid - prev) >= Math.Abs(mid - curr))
-                                tempR = mid;
-                            else tempL = mid;
-                        }
-
-                        l = tempR;
-                    }
-                    if(Math.Abs(r - prev) < Math.Abs(r - curr))
-                    {
-                        var tempR = r;
-                        var tempL = l;
-                        for (var i = 0; i < 100; ++i)
-                        {
-                            var mid = tempL + (tempR - tempL) / 2;
-                            if (Math.Abs(mid - prev) > Math.Abs(mid - curr))
-                                tempL = mid;
-                            else tempR = mid;
-                        }
-
-                        r = tempL;
-                    }
-                    break;
-                }
-                case "further":
-                {
-                    if(Math.Abs(l - prev) > Math.Abs(l - curr))
-                    {
-                        var tempR = r;
-                        var tempL = l;
-                        for (var i = 0; i < 100; ++i)
-                        {
-                            var mid = tempL + (tempR - tempL) / 2;
-                            if (Math.Abs(mid - prev) < Math.Abs(mid - curr))
-                                tempR = mid;
-                            else tempL = mid;
-                        }
-
-                        l = tempR;
-                    }
-                    if(Math.Abs(r - prev) > Math.Abs(r - curr))
-                    {
-                        var tempR = r;
-                        var tempL = l;
-                        for (var i = 0; i < 100; ++i)
-                        {
-                            var mid = tempL + (tempR - tempL) / 2;
-                            if (Math.Abs(mid - prev) < Math.Abs(mid - curr))
-                                tempL = mid;
-                            else tempR = mid;
-                        }
-
-                        r = tempL;
-                    }
-                    break;
-                }
-                
-            }
-
-            prev = curr;
+            Console.WriteLine(l + " " + r);
         }
-        Console.WriteLine(l + " " + r);
     }
 }
